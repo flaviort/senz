a8552cd1cae3f88cae1e324dce0121dd91cb81be
Flávio <flavioczuk@gmail.com>
2022-04-13 11:01:57 -0300

    initial commit

---
+++ includes/head.php
@@ -0,0 +1,32 @@
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="format-detection" content="telephone=no">
+	<link rel="shortcut icon" href="favicon.png">
+
+	<meta name="author" content="Senz Design">
+
+	<title>Senz Design</title>
+
+	<!-- GOOGLE -->
+	<meta name="description" content="Senz Design"/>
+	<link rel="canonical" href="https://senzdsn.com.br/" />
+		
+	<!-- FACEBOOK -->
+	<meta property="og:locale" content="en-US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Senz Design" />
+	<meta property="og:description" content="Senz Design" />
+	<meta property="og:url" content="https://senzdsn.com.br/" />
+	<meta property="og:site_name" content="Senz Design" />
+	<meta property="og:image" content="https://senzdsn.com.br/assets/img/og-image.jpg" />
+	<meta property="og:image:secure_url" content="https://senzdsn.com.br/assets/img/og-image.jpg" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="assets/css/framework.min.css">
+	<link rel="stylesheet" href="assets/css/main.css">
+	
+</head>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+	<!-- HEAD -->
+	<?php include('includes/head.php'); ?>
+
+	<!-- BODY -->
+	<body class="home">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					
+					<h1 class="text">
+						Senz
+					</h1>
+
+				</div><!-- END COL -->
+			</div><!-- END ROW -->
+		</div><!-- END CONTAINER -->
+
+		<!-- FOOTER-->
+		<footer>
+			<script src="assets/js/framework.js"></script>
+		</footer>
+
+	</body><!-- END BODY -->
+</html><!-- END HTML -->
