73dee2044d6efac20131abcc728f9db6b486dd1b
Flávio Roberto Troszczanczuk <flavioczuk@gmail.com>
2022-05-25 11:06:57 -0300

    more fixes

---
+++ .nuxt/dist/server/pages/index.js
@@ -0,0 +1,429 @@
+exports.ids = [4,1];
+exports.modules = {
+
+/***/ 46:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/thumb-lp-fitness.f56c9d9.jpg";
+
+/***/ }),
+
+/***/ 47:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/thumb-lp-fitness@2x.6b2adcb.jpg";
+
+/***/ }),
+
+/***/ 54:
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ 55:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/buttons/ArrowDown.vue?vue&type=template&id=3f95d186&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_vm._ssrNode("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\" fill=\"currentColor\" class=\"w-5 h-5 text-white\"><path fill-rule=\"evenodd\" d=\"M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z\" clip-rule=\"evenodd\"></path></svg>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/buttons/ArrowDown.vue?vue&type=template&id=3f95d186&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(1);
+
+// CONCATENATED MODULE: ./components/buttons/ArrowDown.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "854bddda"
+  
+)
+
+/* harmony default export */ var ArrowDown = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ 62:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/thumb-ultra-capital.c63ea72.jpg";
+
+/***/ }),
+
+/***/ 63:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/thumb-ultra-capital@2x.8ceb7e7.jpg";
+
+/***/ }),
+
+/***/ 64:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_nuxtjs_router_extras_lib_loader_js_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_custom_index_0_blockType_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(54);
+/* harmony import */ var _node_modules_nuxtjs_router_extras_lib_loader_js_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_custom_index_0_blockType_router__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_nuxtjs_router_extras_lib_loader_js_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_custom_index_0_blockType_router__WEBPACK_IMPORTED_MODULE_0__);
+ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_nuxtjs_router_extras_lib_loader_js_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_custom_index_0_blockType_router__WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
+/***/ 71:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=cf2616e8&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_vm._ssrNode("<section class=\"relative flex items-center justify-start pt-40 pb-28 sm:py-52 md:py-40 md:pl-20\">","</section>",[_c('NuxtLink',{staticClass:"absolute z-10 hidden px-8 py-3 text-sm text-white transition-all duration-300 border border-white rounded-full md:inline-block top-6 right-6 hover:bg-white hover:text-black link",attrs:{"to":{name: 'Contact'}}},[_vm._v("\n\t\t\tGet in touch\n\t\t")]),_vm._ssrNode(" "),_c('a',{directives:[{name:"scroll-to",rawName:"v-scroll-to",value:('#projects'),expression:"'#projects'"}],staticClass:"absolute right-5 bottom-5 sm:right-10 sm:bottom-10 md:right-16 md:bottom-16 lg:right-24 lg:bottom-24 z-2 animate-fade-in-out-down",attrs:{"href":"#"}},[_c('ButtonsArrowDown')],1),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"text-white container-wrapper\">","</div>",[_vm._ssrNode("<h1 class=\"xs:text-6xl text-[12vw] font-bold sm:text-7xl lg:text-8xl xl:text-9xl leading-none\"><span>We create</span><br> <span><span class=\"text-white-shadow\">unique</span> digital</span><br> <span>experiences</span></h1> <p class=\"block w-2/3 max-w-full mt-10 mb-12 sm:w-1/2 lg:w-4/12\">\n\t\t\t\tWhether you're a startup or a growing company, we provide everything you need to reach your clients.\n\t\t\t</p> "),_c('NuxtLink',{staticClass:"inline-block px-10 py-4 transition-all duration-300 border border-white rounded-full hover:bg-white hover:text-black",attrs:{"to":{ name: 'Services'}}},[_vm._v("\n\t\t\t\tOur services\n\t\t\t")])],2)],2),_vm._ssrNode(" "),_vm._ssrNode("<section id=\"projects\" class=\"relative pt-24 pb-32 text-white md:pl-20 md:py-52\">","</section>",[_vm._ssrNode("<div class=\"container-wrapper\">","</div>",[_vm._ssrNode("<h3 class=\"mb-20 text-5xl font-bold sm:mb-32 sm:text-6xl lg:text-7xl md:mb-72 projects-title mix-blend-difference\"><span>Featured</span><br> <span class=\"text-white-shadow\">projects</span></h3> "),_vm._ssrNode("<div class=\"relative flex projects\">","</div>",[_vm._ssrNode("<div class=\"w-full md:w-1/2 description md:pt-[25vh] relative z-2 text-black\">","</div>",[_vm._ssrNode("<div class=\"items-center w-full mb-14 md:mb-0 md:flex md:h-screen md:w-3/4 xl:w-2/3\">","</div>",[_vm._ssrNode("<div class=\"project-infos-01\">","</div>",[_c('NuxtLink',{staticClass:"block mb-7 md:hidden",attrs:{"to":{ name: 'LP Fitness' }}},[_c('picture',{staticClass:"relative h-0 pb-[90%] w-full block"},[_c('img',{staticClass:"absolute object-cover object-top w-full h-full",attrs:{"src":__webpack_require__(46),"srcset":__webpack_require__(47) + " 2x","alt":"LP Fitness"}})])]),_vm._ssrNode(" <p class=\"text-base sm:text-xl md:text-2xl\">\n\t\t\t\t\t\t\t\tLP Fitness\n\t\t\t\t\t\t\t</p> <h2 class=\"block mt-1 mb-4 text-2xl font-bold md:mt-4 md:mb-6 sm:text-3xl md:text-4xl\">\n\t\t\t\t\t\t\t\tOnline couching and personal trainer\n\t\t\t\t\t\t\t</h2> <p class=\"text-sm md:text-base\">\n\t\t\t\t\t\t\t\tbranding, web\n\t\t\t\t\t\t\t</p>")],2)]),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"items-center w-full mb-14 md:mb-0 md:flex md:h-screen md:w-3/4 xl:w-2/3\">","</div>",[_vm._ssrNode("<div class=\"project-infos-02\">","</div>",[_c('NuxtLink',{staticClass:"block mb-7 md:hidden",attrs:{"to":{ name: 'LP Fitness' }}},[_c('picture',{staticClass:"relative h-0 pb-[90%] w-full block"},[_c('img',{staticClass:"absolute object-cover object-top w-full h-full",attrs:{"src":__webpack_require__(46),"srcset":__webpack_require__(47) + " 2x","alt":"LP Fitness"}})])]),_vm._ssrNode(" <p class=\"text-base sm:text-xl md:text-2xl\">\n\t\t\t\t\t\t\t\tUltra Capital\n\t\t\t\t\t\t\t</p> <h2 class=\"block mt-1 mb-4 text-2xl font-bold md:mt-4 md:mb-6 sm:text-3xl md:text-4xl\">\n\t\t\t\t\t\t\t\tLorem, ipsum dolor sit amet consectetur adipisicing\n\t\t\t\t\t\t\t</h2> <p class=\"text-sm md:text-base\">\n\t\t\t\t\t\t\t\tweb\n\t\t\t\t\t\t\t</p>")],2)]),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"md:flex items-center w-full md:h-screen md:w-3/4 xl:w-2/3 md:mb-[20vh] mb-14 md:mb-20\">","</div>",[_vm._ssrNode("<div class=\"project-infos-03\">","</div>",[_c('NuxtLink',{staticClass:"block mb-7 md:hidden",attrs:{"to":{ name: 'LP Fitness' }}},[_c('picture',{staticClass:"relative h-0 pb-[90%] w-full block"},[_c('img',{staticClass:"absolute object-cover object-top w-full h-full",attrs:{"src":__webpack_require__(46),"srcset":__webpack_require__(47) + " 2x","alt":"LP Fitness"}})])]),_vm._ssrNode(" <p class=\"text-base sm:text-xl md:text-2xl\">\n\t\t\t\t\t\t\t\tMender\n\t\t\t\t\t\t\t</p> <h2 class=\"block mt-1 mb-4 text-2xl font-bold md:mt-4 md:mb-6 sm:text-3xl md:text-4xl\">\n\t\t\t\t\t\t\t\tFind the perfect professional for you\n\t\t\t\t\t\t\t</h2> <p class=\"text-sm md:text-base\">\n\t\t\t\t\t\t\t\tweb\n\t\t\t\t\t\t\t</p>")],2)])],2),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"hidden md:block project-image h-[90vh] w-1/2 xl:w-5/12 overflow-hidden float-right\">","</div>",[_vm._ssrNode("<div class=\"relative w-full h-full\">","</div>",[_vm._ssrNode("<div class=\"absolute top-0 left-0 block w-full h-full overflow-hidden project-link-01 bg-slate-300 z-3\">","</div>",[_c('NuxtLink',{staticClass:"block w-full h-full transition-transform duration-1000 project-link hover:scale-105",attrs:{"to":{ name: 'LP Fitness' }}},[_c('picture',[_c('img',{staticClass:"object-cover w-full h-full",attrs:{"src":__webpack_require__(46),"srcset":__webpack_require__(47) + " 2x","alt":"LP Fitness"}})])])],1),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"absolute top-0 left-0 block w-full h-full overflow-hidden project-link-02 bg-slate-300 z-2\">","</div>",[_c('NuxtLink',{staticClass:"block w-full h-full transition-transform duration-1000 project-link hover:scale-105",attrs:{"to":{ name: 'Ultra Capital' }}},[_c('picture',[_c('img',{staticClass:"object-cover w-full h-full",attrs:{"src":__webpack_require__(62),"srcset":__webpack_require__(63) + " 2x","alt":"LP Fitness"}})])])],1),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"absolute top-0 left-0 block w-full h-full overflow-hidden project-link-03 bg-slate-300 z-1\">","</div>",[_c('NuxtLink',{staticClass:"block w-full h-full transition-transform duration-1000 project-link hover:scale-105",attrs:{"to":{ name: 'Contact' }}},[_vm._v("\n\t\t\t\t\t\t\t\tccccc\n\t\t\t\t\t\t\t")])],1)],2)])],2),_vm._ssrNode(" "),_vm._ssrNode("<div class=\"block md:text-center md:pt-28 md:pb-52\">","</div>",[_c('NuxtLink',{staticClass:"inline-block px-10 py-5 text-black transition-all duration-300 border border-black rounded-full hover:bg-black hover:text-white",attrs:{"to":{ name: 'Projects' }}},[_vm._v("\n\t\t\t\t\tView all projects\n\t\t\t\t")])],1)],2)]),_vm._ssrNode(" "),_vm._ssrNode("<section id=\"about\" class=\"relative pb-32 text-black md:pl-20 md:pb-52\">","</section>",[_vm._ssrNode("<div class=\"container-wrapper\">","</div>",[_vm._ssrNode("<p class=\"block mb-6 text-base md:mb-10 md:text-lg\"><strong>\n\t\t\t\t\tAbout us\n\t\t\t\t</strong></p> <p class=\"block text-xl !leading-normal md:w-2/3 sm:text-2xl lg:text-4xl\">\n\t\t\t\tWe are a digital service and consulting firm which focuses on catering to the demands of our clients by offering them innovate and quality driven solutions.\n\t\t\t</p> "),_vm._ssrNode("<div class=\"block pt-10 -ml-1 md:pt-12\">","</div>",[_c('NuxtLink',{staticClass:"inline-block px-10 py-5 text-black transition-all duration-300 border border-black rounded-full hover:bg-black hover:text-white",attrs:{"to":{ name: 'About' }}},[_c('span',[_vm._v("\n\t\t\t\t\t\tGet to know us\n\t\t\t\t\t")])])],1)],2)])],2)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=cf2616e8&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+const vh = coef => window.innerHeight * (coef / 100);
+
+/* harmony default export */ var lib_vue_loader_options_pagesvue_type_script_lang_js_ = ({
+  data() {
+    return {
+      title: "Senz®"
+    };
+  },
+
+  head() {
+    return {
+      title: this.title,
+      meta: [{
+        hid: "description",
+        name: "description",
+        content: "Whether you're a startup or a growing company, we provide everything you need to reach your clients."
+      }]
+    };
+  },
+
+  methods: {
+    animateOnScroll() {
+      this.$ScrollTrigger.create({
+        trigger: '.projects .project-image',
+        start: vh(-5) + 'top',
+        end: vh(245) + 'top',
+        pin: true
+      });
+    },
+
+    bgColor() {
+      let tl = this.$gsap.timeline({
+        paused: true
+      });
+      tl.to('body', {
+        backgroundColor: '#fff'
+      });
+      this.$ScrollTrigger.create({
+        trigger: '.projects-title',
+        start: '-300 top',
+        end: '300 top',
+        scrub: true,
+        animation: tl //markers: true,
+
+      });
+    },
+
+    projectImagesScroll01() {
+      let tl = this.$gsap.timeline({
+        paused: true
+      });
+      tl.to('.project-link-01', {
+        autoAlpha: '0'
+      });
+      this.$ScrollTrigger.create({
+        trigger: '.project-infos-01',
+        start: 'top top',
+        end: vh(30) + 'top',
+        scrub: true,
+        animation: tl
+      });
+    },
+
+    projectImagesScroll02() {
+      let tl = this.$gsap.timeline({
+        paused: true
+      });
+      tl.to('.project-link-02', {
+        autoAlpha: '0'
+      });
+      this.$ScrollTrigger.create({
+        trigger: '.project-infos-02',
+        start: 'top top',
+        end: vh(30) + 'top',
+        scrub: true,
+        animation: tl
+      });
+    }
+
+  },
+
+  mounted() {
+    this.animateOnScroll();
+    this.projectImagesScroll01();
+    this.projectImagesScroll02();
+    this.bgColor();
+  },
+
+  beforeRouteLeave(from, to, next) {
+    this.$nextTick(() => {
+      this.$gsap.to('body', {
+        clearProps: 'all'
+      });
+      this.$ScrollTrigger.getAll().forEach(ST => ST.kill());
+      next();
+    });
+  }
+
+});
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pagesvue_type_script_lang_js_ = (lib_vue_loader_options_pagesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(1);
+
+// EXTERNAL MODULE: ./pages/index.vue?vue&type=custom&index=0&blockType=router
+var pagesvue_type_custom_index_0_blockType_router = __webpack_require__(64);
+
+// CONCATENATED MODULE: ./pages/index.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pagesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "384f18e8"
+  
+)
+
+/* custom blocks */
+
+if (typeof pagesvue_type_custom_index_0_blockType_router["default"] === 'function') Object(pagesvue_type_custom_index_0_blockType_router["default"])(component)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/* nuxt-component-imports */
+installComponents(component, {ButtonsArrowDown: __webpack_require__(55).default})
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
